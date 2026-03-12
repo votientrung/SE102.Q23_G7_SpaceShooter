34c5d79c04ce7e1e1133374f93e91143a56866ae
Huỳnh Gia Phúc <bin342558@gmail.com>
2026-03-12 19:36:03 +0700

    bullet animation

--- space_shooter/script/player_bullets.gd
+++ space_shooter/script/player_bullets.gd
@@ -1,5 +1,7 @@
 extends Area2D
 class_name bullets
+
+@onready var ha= $AnimatedSprite2D
 @export var damage : float = 1
 @export var speed : float = 300
 @export var direction : Vector2
@@ -14,8 +16,15 @@ func _process(delta):
 		queue_free()
 	if global_position.x < -10:
 		queue_free()
+	# handle animation
+	handle_animation()
 
 func _on_body_entered(body: CharacterBody2D) -> void:
 	if body.has_method("damage_take"):
 			body.damage_take(damage)
 			queue_free()
+
+
+
+func handle_animation():
+	ha.play("default")
