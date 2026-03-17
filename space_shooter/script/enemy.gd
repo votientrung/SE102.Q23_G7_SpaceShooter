996d2f63945b76912e28d4b873e5d0764e76ff2f
votientrung <163385850+votientrung@users.noreply.github.com>
2026-03-17 21:31:18 +0700

    fix bug

--- space_shooter/script/enemy.gd
+++ space_shooter/script/enemy.gd
@@ -51,6 +51,11 @@ func _process(delta: float) -> void:
 		velocity = Vector2.ZERO
 	else:
 		return
+		if global_position.distance_to(target_position) < 5:
+			global_position = target_position
+			is_entering = false
+			velocity = Vector2.ZERO
+
 	
 	#skill
 	if skill != null and !is_entering:
